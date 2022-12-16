218be22ca62648d06e5d4772b0b46aea1fcade70
JakubSzczerba <jakszczerb@gmail.com>
2022-12-16 17:21:10 +0100

    Basic form and render it.

---
+++ src/Controller/DashboardController.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+ * This file was created by Jakub Szczerba
+ * Contact: https://www.linkedin.com/in/jakub-szczerba-3492751b4/
+*/
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use App\Form\ApplicationType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class DashboardController extends AbstractController
+{
+    #[Route('/dashboard', name: 'dashboard')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ApplicationType::class);
+        $form->handleRequest($request);
+
+        return $this->render('dashboard/index.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
---
+++ src/Form/ApplicationType.php
@@ -0,0 +1,31 @@
+<?php
+
+/*
+ * This file was created by Jakub Szczerba
+ * Contact: https://www.linkedin.com/in/jakub-szczerba-3492751b4/
+*/
+
+declare(strict_types=1);
+
+namespace App\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class ApplicationType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('topic', TextType::class, [
+                'label' => 'Temat: ',
+            ])
+            ->add('comment', TextareaType::class, [
+                'label' => 'Treść: ',
+            ])
+        ;
+    }
+
+}
