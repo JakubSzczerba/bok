3820092a32c04d381098c6fcdf3ba1f395186701
JakubSzczerba <jakszczerb@gmail.com>
2022-12-27 15:25:46 +0100

    modify logic for set first read user and upgrade single application view

--- src/Controller/BokController.php
+++ src/Controller/BokController.php
@@ -12,6 +12,7 @@ namespace App\Controller;
 use App\Entity\Application;
 use App\Provider\ApplicationProvider;
 use App\Repository\ApplicationRepository;
+use App\Services\Application\ReadApplication;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -22,12 +23,15 @@ class BokController extends AbstractController
 {
     private ApplicationProvider $applicationProvider;
 
+    private ReadApplication $readApplication;
+
     private EntityManagerInterface $entityManager;
 
-    public function __construct(ApplicationProvider $applicationProvider, EntityManagerInterface $entityManager)
+    public function __construct(ApplicationProvider $applicationProvider, ReadApplication $readApplication, EntityManagerInterface $entityManager)
     {
         $this->applicationProvider = $applicationProvider;
         $this->entityManager = $entityManager;
+        $this->readApplication = $readApplication;
     }
 
     #[Route('/bok', name: 'bok')]
@@ -44,11 +48,13 @@ class BokController extends AbstractController
     public function singleApplication(Request $request, int $id): Response
     {
         $application = $this->applicationProvider->getSingleData($id);
-        $application->setUser($this->getUser());
-        $application->setIsRead(true);
-        $application->setDateRead(new \DateTime());
 
-        $this->entityManager->flush();
+        if($application->getUser() === null) {
+            $application->setUser($this->getUser());
+            $this->readApplication->updateData($application);
+
+            $this->entityManager->flush();
+        }
 
         return $this->render('Bok/Application/index.html.twig', [
             'application' => $application,
